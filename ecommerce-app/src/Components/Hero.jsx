4700b1195d484db3cedec237013a74e23ce0a713
DaFlusher <joyibe5@gmail.com>
2023-12-14 17:29:20 +0100

    yet another change to the img file path

--- ecommerce-app/src/Components/Hero.jsx
+++ ecommerce-app/src/Components/Hero.jsx
@@ -8,10 +8,10 @@ import thumbnailOne from '../images/image-product-1-thumbnail.jpg';
 import thumbnailTwo from '../images/image-product-2-thumbnail.jpg';
 import thumbnailThree from '../images/image-product-3-thumbnail.jpg';
 import thumbnailFour from '../images/image-product-1-thumbnail.jpg';
-import productOne from '../images/image-product-1.jpg';
-import productTwo from '../images/image-product-2.jpg';
-import productThree from '../images/image-product-3.jpg';
-import productFour from '../images/image-product-4.jpg';
+import productOne from '/src/images/image-product-1.jpg';
+import productTwo from '/src/images/image-product-2.jpg';
+import productThree from '/src/images/image-product-3.jpg';
+import productFour from '/src/images/image-product-4.jpg';
 import close from '../images/icon-close.svg';
 import previous from '../images/icon-previous.svg';
 import next from '../images/icon-next.svg';
@@ -56,10 +56,10 @@ const Hero = () => {
              close={() => setOpen(false)}
              slides={[
                 
-               { src: "../images/image-product-1.jpg" },
-               { src: "../images/image-product-2.jpg" },
-               { src: "../images/image-product-3.jpg" },
-               { src: "../images/image-product-4.jpg" },
+               { src: "/src/images/image-product-1.jpg" },
+               { src: "/src/images/image-product-2.jpg" },
+               { src: "/src/images/image-product-3.jpg" },
+               { src: "/src/images/image-product-4.jpg" },
              ]}
                     plugins={[Thumbnails]}
             thumbnails={{ ref: thumbnailsRef }}
